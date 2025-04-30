f7e999c4b9f0a9d5f2e4bfa4e4c983382080d946
Steven de Valk <steven.devalk@atos.net>
2025-04-30 10:50:21 +0200

    Moved main.css contents to App.vue;

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,19 @@
 </template>
 
 <style scoped>
+  body {
+    margin: 0;
+    overflow: hidden;
+  }
+
+  header {
+    background-color: #0073e6;
+    color: white;
+  }
+
+  iframe {
+    border: 0;
+    height: 100vh;
+    width: 100%;
+  }
 </style>
