9f1fd5415e975e8be27d40de661227dd2b3cdaee
Steven de Valk <steven.devalk@atos.net>
2025-04-30 11:14:54 +0200

    Moved body reset styling to main.css; Added logo and curves to header;

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <header>
-    Atos Wordcloud
+    <div id="logo-wrapper">
+      <img id="logo" src="@/assets/atos_logo_white.svg" />
+    </div>
+    <img id="header-curves" src="@/assets/header-curves.svg" />
   </header>
   <iframe src="https://www.example.com"></iframe>
 </template>
 
 <style scoped>
-  body {
-    margin: 0;
-    overflow: hidden;
-  }
-
   header {
     background-color: #0073e6;
     color: white;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
   }
+
+  #logo-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center ;
+  }
+
+  #logo {
+    height: 2em;
+    margin: 1em;
+  }
+
+  #header-curves {
+    max-height: 5em;
+  }
+
+
 
   iframe {
     border: 0;
